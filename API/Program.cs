09ea8f05ab0fea0c35b19c29931d304c5a1ff6ef
Mustafa shawkey <92957187+Mustafa212@users.noreply.github.com>
2024-06-26 19:04:11 +0300

    "angular skeleton"

--- API/Program.cs
+++ API/Program.cs
@@ -3,28 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Ensure that the configuration is built
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                     .AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data source = dating.db"));
 
 builder.Services.AddControllers();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "http://localhost:4200","https://localhost:4200"
+));
 
 app.MapControllers();
 
